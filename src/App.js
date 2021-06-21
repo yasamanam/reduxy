fa58435ac33f6f587849870226f9af1564d95713
yasaman rezaeeshirsavar <yasaman@dineeasy.ca>
2021-06-21 22:15:55 +0430

    add files

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
+
+import Header from "./containers/Header";
+import ProductDetail from "./containers/ProductDetail";
+import ProductListing from "./containers/ProductListing";
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Header />
+        <Switch>
+          <Route path="/" exact component={ProductListing} />
+          <Route path="/product/:productId" exact component={ProductDetail} />
+          <Route>404 Not Found</Route>
+        </Switch>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/containers/ProductListing.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { useSelector } from "react-redux";
+
+const ProductListing = () => {
+  const products = useSelector((state) => state.allProducts);
+
+  console.log(products);
+  return (
+    <div className="ui grid container">
+      <h1> Product Listing</h1>
+    </div>
+  );
+};
+
+export default ProductListing;
